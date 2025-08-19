e6f71e5e3f41ea40abb9390866489611a4f94a7d
aryanrafsanjany <aryanrafsanjany@gmail.com>
2025-08-19 19:11:14 +0600

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5001/api";
+const API_BASE_URL = "https://dhaka-mall-parking-system.onrender.com/api";
 
 // Helper function to get auth headers
 const getAuthHeaders = () => {
